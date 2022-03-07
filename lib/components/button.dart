0f3720830774a926029a2b1a5c37881399dfd040
Mohamed Akeel <akeel1447@gmail.com>
2022-03-07 10:17:01 +0530

    Initialize Flutter Project

---
+++ lib/components/button.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  static const String routeName = '/';
+  const Button({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Button();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:ctse_flutter_project/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CTSE Flutter Project',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: Home.routeName,
+      routes: {
+        Home.routeName: ((context) => const Home()),
+      },
+    );
+  }
+}

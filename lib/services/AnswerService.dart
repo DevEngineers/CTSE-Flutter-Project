5bacbc3c862cf0428bce2ed643ab43a5d880f394
Mohamed Akeel <akeel1447@gmail.com>
2022-03-28 22:41:52 +0530

    fixing get answer service function issue

--- lib/services/AnswerService.dart
+++ lib/services/AnswerService.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart';
 import '../model/answer.dart';
@@ -29,18 +30,28 @@ class AnswerService {
   const AnswerService();
 
   Future<List<Answer>> getStoredAnswers() async {
-    // final response = await post(Uri.parse(endpoint),
-    //     headers: {
-    //       'Content-type': 'application/json',
-    //       'Accept': 'application/json',
-    //     },
-    //     body: json.encode(answers));
+    final response = await get(Uri.parse(endpoint), headers: {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+    });
 
-    // if (response.statusCode == 200) {
-    //   return true;
-    // }
-    // throw Exception('Error in getting the age');
-    return q;
+    if (response.statusCode == 200) {
+      List<Answer> answerList = [];
+      List<dynamic> data = jsonDecode(response.body);
+
+      for (dynamic item in data) {
+        Answer answer = Answer(
+            id: item['_id'],
+            questionId: item['questionId'],
+            topicId: item['topicId'],
+            answer: item['answer'],
+            isCorrect: item['isCorrect']);
+        answerList.add(answer);
+      }
+
+      return answerList;
+    }
+    throw Exception('Error in getting the answers');
   }
 
   Future<bool?> submitAnswers(List<Answer> answers) async {
@@ -54,7 +65,21 @@ class AnswerService {
     if (response.statusCode == 200) {
       return true;
     }
-    throw Exception('Error in getting the age');
+    throw Exception('Error in Submitting the answers');
+  }
+
+  Future<bool?> updateAnswers(List<Answer> answers) async {
+    final response = await put(Uri.parse(endpoint),
+        headers: {
+          'Content-type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: json.encode(answers));
+
+    if (response.statusCode == 200) {
+      return true;
+    }
+    throw Exception('Error in updating the answers');
   }
 
   Future<bool?> deleteAllQuizzes() async {
